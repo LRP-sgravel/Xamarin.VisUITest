e0777da518901d72d2589a386ff5b190210e02c0
Sylvain Gravel <sgravel@lerondpoint.net>
2016-01-26 11:47:46 -0500

    Added options class with reference and current images path

---
+++ Xamarin.VisUITest/VisUITest.cs
@@ -0,0 +1,37 @@
+﻿namespace Xamarin.VisUITest
+{
+    public class VisUITest
+    {
+        private string mReferenceImagePath = "./_output/VisUITest/ref/";
+        public static string ReferenceImagePath
+        {
+            get { return Instance.mReferenceImagePath; }
+            set { Instance.mReferenceImagePath = value; }
+        }
+
+        private string mCurrentImagePath = "./_output/VisUITest/";
+        public static string CurrentImagePath
+        {
+            get { return Instance.mCurrentImagePath; }
+            set { Instance.mCurrentImagePath = value; }
+        }
+
+        private static VisUITest mInstance = null;
+        private static VisUITest Instance
+        {
+            get
+            {
+                if (mInstance == null)
+                {
+                    mInstance = new VisUITest();
+                }
+
+                return mInstance;
+            }
+        }
+
+        private VisUITest()
+        {
+        }
+    }
+}
