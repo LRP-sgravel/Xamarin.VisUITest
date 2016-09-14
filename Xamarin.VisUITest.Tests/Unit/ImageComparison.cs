71a8a5f6f0028f61192480d5d5723f814ee07117
Sylvain Gravel <sgravel@lerondpoint.net>
2016-09-14 13:28:55 -0400

    Changed image in unsupported format for comparison

--- Xamarin.VisUITest.Tests/Unit/ImageComparison.cs
+++ Xamarin.VisUITest.Tests/Unit/ImageComparison.cs
@@ -13,22 +13,23 @@ namespace Xamarin.VisUITest.Tests
             Bitmap imageA = Imaging.Image.FromFile("../../../img/A.jpg");
             Bitmap imageB = Imaging.Image.FromFile("../../../img/B.png");
             Bitmap imageC = Imaging.Image.FromFile("../../../img/C.png");
-            Bitmap imageD = Imaging.Image.FromFile("../../../img/D.jpg");/*
-            Bitmap imageE = Imaging.Image.FromFile("../../../img/E.gif");
+            Bitmap imageD = Imaging.Image.FromFile("../../../img/D.jpg");
+            Bitmap imageE = Imaging.Image.FromFile("../../../img/E.png");
             Bitmap imageF = Imaging.Image.FromFile("../../../img/F.tif");
-            Bitmap imageG = Imaging.Image.FromFile("../../../img/G.jpg");*/
+            Bitmap imageG = Imaging.Image.FromFile("../../../img/G.jpg");
 
             Assert.IsTrue(imageA.IsIdenticalTo(imageA));
             Assert.IsTrue(imageA.IsIdenticalTo(imageC));
-            Assert.IsFalse(imageA.IsIdenticalTo(imageB));/*
+            Assert.IsFalse(imageA.IsIdenticalTo(imageB));
             Assert.IsFalse(imageA.IsIdenticalTo(imageF));
 
-            Assert.IsTrue(imageD.IsIdenticalTo(imageE));
+            Assert.IsFalse(imageD.IsIdenticalTo(imageE));
+            Assert.IsTrue(imageD.IsIdenticalTo(imageE, 1));
             Assert.IsFalse(imageD.IsIdenticalTo(imageA));
             Assert.IsFalse(imageD.IsIdenticalTo(imageG));
 
             Assert.IsTrue(imageF.IsIdenticalTo(imageG));
-            Assert.IsFalse(imageF.IsIdenticalTo(imageB));*/
+            Assert.IsFalse(imageF.IsIdenticalTo(imageB));
         }
     }
 }
