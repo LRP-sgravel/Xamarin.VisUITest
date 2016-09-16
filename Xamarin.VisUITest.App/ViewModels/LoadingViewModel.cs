1067c3a34f72fea2c18702f4a62094fb1a79d86d
Sylvain Gravel <sgravel@lerondpoint.net>
2016-09-16 14:15:40 -0400

    Forgotten file

--- Xamarin.VisUITest.App/ViewModels/LoadingViewModel.cs
+++ Xamarin.VisUITest.App/ViewModels/LoadingViewModel.cs
@@ -1,53 +1,16 @@
-﻿using System;
-using MvvmCross.Core.ViewModels;
-using MvvmCross.Localization;
-using MvvmCross.Platform;
-using Xamarin.VisUITest.App.Services;
+﻿using MvvmCross.Core.ViewModels;
 
 namespace Xamarin.VisUITest.App.ViewModels
 {
     class LoadingViewModel : MvxViewModel
     {
-        private Bootstrapper Bootstrapper { get; set; }
-
-        public string CopyrightText { get; set; }
-
-        private string mStatusText = string.Empty;
-        public string StatusText
-        {
-            get { return mStatusText; }
-            private set
-            {
-                if (StatusText != value)
-                {
-                    mStatusText = value;
-                    RaisePropertyChanged(() => StatusText);
-                }
-            }
-        }
-
         public LoadingViewModel()
         {
-            IMvxTextProvider textProvider = Mvx.Resolve<IMvxTextProvider>();
-
-            Bootstrapper = new Bootstrapper();
-
-            CopyrightText = textProvider.GetText(Constants.GeneralNamespace, Constants.TextTypeKey, "Copyright");
         }
 
         public override void Start()
         {
             base.Start();
-
-            Bootstrapper.BootTextChanged += status => { StatusText = status; };
-            Bootstrapper.BootCompleted += OnBootCompleted;
-
-            Bootstrapper.Boot();
-        }
-
-        public void OnBootCompleted()
-        {
-
         }
     }
 }
