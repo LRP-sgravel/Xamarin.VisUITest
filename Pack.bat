f9ba174e8c0c6c181a90525907f8d4adb72a682a
Sylvain Gravel <sgravel@lerondpoint.net>
2017-06-05 22:22:41 -0400

    Moved nuspec to their own folder & created pack script

---
+++ Pack.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\NuGet\NuGet.exe" pack nuspec/Xamarin.VisUITest.nuspec -OutputDirectory nuspec
+"C:\Program Files (x86)\NuGet\NuGet.exe" pack nuspec/Xamarin.VisUITest.Backdoors.Droid.nuspec -OutputDirectory nuspec
+"C:\Program Files (x86)\NuGet\NuGet.exe" pack nuspec/Xamarin.VisUITest.Backdoors.Forms.Droid.nuspec -OutputDirectory nuspec
+"C:\Program Files (x86)\NuGet\NuGet.exe" pack nuspec/Xamarin.VisUITest.Backdoors.Mvx.Droid.nuspec -OutputDirectory nuspec
